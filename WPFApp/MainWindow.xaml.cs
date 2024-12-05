93c27e79578d1cf81498754b1948e09d47db037f
Sebastián Carrera Giraldo <sebastian.carrerag123@gmail.com>
2024-12-05 13:17:09 -0500

    Creación Task 1 y Task 2

--- WPFApp/MainWindow.xaml.cs
+++ WPFApp/MainWindow.xaml.cs
@@ -1,24 +1,33 @@
-﻿using System.Text;
-using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+﻿using System.Windows;
 
-namespace WPFApp
+namespace WPFApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
-    public partial class MainWindow : Window
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    void CreateTask()
+    {
+        Task T;
+        Action Code = new Action(ShowMessage);
+        T = new Task(Code);
+        Task T2 = new Task(delegate
         {
-            InitializeComponent();
+            MessageBox.Show("Ejecutando una tarea en un método anínimo");
+        }
+        );  
+        
+    }
+
+        void ShowMessage()
+        {
+        MessageBox.Show("Ejecutando el método ShowMessage");
+
         }
     }
-}
